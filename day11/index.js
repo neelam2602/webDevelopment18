e6c554009ccb4ac6f6563572c4cc9f641e127952
neelam2602 <37776220+neelam2602@users.noreply.github.com>
2018-04-17 16:01:45 +0530

    Add files via upload

--- day11/index.js
+++ day11/index.js
@@ -64,6 +64,43 @@ $(document).ready(function(){
 	})
 	// tab examples
 
+	// slider examples
+	slide_wi = $(".slider").width();
+	// alert(slide_wi);
+	slide_hi = Math.round($(".slider").height());
+	// alert(slide_hi);
+
+	$(".slider").css("float","left");
+	$(".mainslide").width(slide_wi * $(".slider").length);
+	$(".mainslide, .gallery").height(slide_hi);
+	$(".gallery").width(slide_wi);
+	// $(".gallery").css("border","3px solid");
+	$(".gallery").css("overflow","hidden");
+	// $(".mainslide").css("border","3px solid red")
+
+	slider_arr = [];
+	$(".slider").each(function(position){
+		// console.log(position);
+		// console.log($(this).width())
+		// console.log($(this).width() *position)
+
+		slider_arr[position] = $(this).width() * position;
+
+	})
+	// console.log(slider_arr)
+	$(".circles span").click(function(){
+		var pos = $(this).index();
+		// console.log(pos)
+		// console.log(slider_arr[pos])
+		$(".mainslide").animate({
+			"margin-left": -slider_arr[pos]+"px"
+		},1000);
+		$(".circles span").css("background","#f1f1f1")
+		$(this).css("background","#c1c1c1")
+
+	})
+	
+	// slider examples
 
 
 })
