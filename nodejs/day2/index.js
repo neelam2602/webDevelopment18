548a1d3ff93789682ae5fb616db96322b9dedb34
neelam2602 <37776220+neelam2602@users.noreply.github.com>
2018-04-26 15:19:40 +0530

    Add files via upload

--- nodejs/day2/index.js
+++ nodejs/day2/index.js
@@ -2,6 +2,20 @@ var express = require('express');
 // console.log(express);
 var mysql = require('mysql');
 var bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+        host: 'mail.php-training.in',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'vishal@php-training.in', // generated ethereal user
+            pass: 'Vishal@123' // generated ethereal password
+        },tls: {
+        rejectUnauthorized:false
+    }
+    });
+
 
 var connection = mysql.createConnection({
 	host : "localhost",
@@ -13,6 +27,7 @@ var connection = mysql.createConnection({
 
 var app = express();
 // console.log(app)
+app.use('/xyz', express.static(__dirname + '/public'));
 app.set('view engine','pug');
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -42,6 +57,7 @@ app.get('/about',function(req,res){
 app.post('/form_action',function(req,res){
 	// console.log(req.body);
 	data1 = req.body.ex1;
+	// console.log(data1)
 	data2 = req.body.ex2;
 	connection.query("insert into brands (name,count) values ('"+data1+"','"+data2+"')",function(err,result){
 		if(err){
@@ -53,4 +69,44 @@ app.post('/form_action',function(req,res){
 		}
 	})
 })
+app.get('/delete/:urlData',function(req,res){
+	// console.log(req);
+	id = req.params.urlData;
+	connection.query("delete from brands where id = '"+id+"'",function(err,result){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.redirect('/')
+		}
+	})
+})
+app.post('/mail_action',function(req,res){
+	// res.send("hello")
+	// res.send(req.body.ex1);
+	email = req.body.ex1;
+	subject = req.body.ex2;
+	msg = req.body.ex3;
+	let mailOptions = {
+        from: '"vishal" <vishal@php-training.in>', // sender address
+        to: email, // list of receivers
+        subject: subject, // Subject line
+        text: msg, // plain text body
+        html: msg // html body
+    };
+
+	transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.send('mail sent')
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+})
+
 app.listen(3000);
