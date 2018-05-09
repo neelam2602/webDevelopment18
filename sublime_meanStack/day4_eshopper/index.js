1c2c6c9ae16be145741b5e613c78141518a76c98
neelam2602 <37776220+neelam2602@users.noreply.github.com>
2018-05-09 15:07:50 +0530

    Add files via upload

--- sublime_meanStack/day4_eshopper/index.js
+++ sublime_meanStack/day4_eshopper/index.js
@@ -1,5 +1,10 @@
-var express = require('express');
+ var express = require('express');
 var bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
 
 var app = express();
 
@@ -7,12 +12,65 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/public', express.static(__dirname + '/public'));
+let transporter = nodemailer.createTransport({
+        host: 'mail.php-training.in',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "vishal@php-training.in", // generated ethereal user
+            pass: "Vishal@123" // generated ethereal password
+        },
+        tls:{rejectUnauthorized: false}
+
+    });
 
 const db = require('monk')('127.0.0.1:27017/mongo_first')
 // console.log(db)
 // const users = db.get('person')
 
+app.get('/login',function(req,res){
+	res.render('login')
+})
+app.post('/register_action',function(req,res){
+	// res.send("test")
+	var record = req.body
+	// console.log(record)
+	db.get("user_info").insert(record,function(err,result){
+				if(!err){
+					res.send("user added")
+				}
+	});
+					let mailOptions = {
+		        from: '"vishal" <vishal@php-training.in>', // sender address
+		        to: req.body.emailId, // list of receivers
+		        subject: 'registration process', // Subject line
+		        text: 'Hello world?', // plain text body
+		        html: 'Validate yourself' // html body
+		   	 };
+	transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        
 
+
+			
+		
+		else{
+			console.log(err)
+		}
+	})
+	// res.send(record);
+	var mob_num = req.body.mobileNumber;
+	var msg = "validation for eshopper";
+	client.get("http://api.textlocal.in/send/?username=neelamsoni854@gmail.com&hash=e8851cb7cbf3753c3f1980012abb71ed5a855bd44eb13ae5a6ee8dc847ef4d2d&message="+msg+"&sender=TXTLCL&numbers=91"+mob_num+"&test=0", function (data, response) {
+    // parsed response body as js object
+    console.log(data);
+    // raw response
+    console.log(response);
+});
+
+})
 app.get('/',function(req,res){
 
 	db.get('brands').find({},function(err,result){
