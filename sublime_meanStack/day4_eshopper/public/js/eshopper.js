fb75fc73c2ec24c264b0ca0419c2a66d80083358
neelam2602 <37776220+neelam2602@users.noreply.github.com>
2018-05-10 15:44:33 +0530

    Add files via upload

--- sublime_meanStack/day4_eshopper/public/js/eshopper.js
+++ sublime_meanStack/day4_eshopper/public/js/eshopper.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
 		$.post("/register_action",$("#register_action").serialize(),function(res){
 			// alert(res)
 			$(".err_signup").html(res);
+
+		})
+	})
+	$(".btn_login_action").click(function(){
+		// alert("success")
+		$.post("/login_action",$("#login_action").serialize(),function(res){
+			// alert(res)
+			$(".err_div").html(res);
+
 		})
 	})
 })
