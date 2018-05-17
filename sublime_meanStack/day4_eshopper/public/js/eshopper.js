c3bc5815e3cd8bd1239adf422e396f89c2922300
neelam2602 <37776220+neelam2602@users.noreply.github.com>
2018-05-17 15:45:47 +0530

    Add files via upload

--- sublime_meanStack/day4_eshopper/public/js/eshopper.js
+++ sublime_meanStack/day4_eshopper/public/js/eshopper.js
@@ -22,6 +22,37 @@ $(document).ready(function(){
 
 		})
 	})
+	$(".brand").click(function(Bobj){
+		// alert("brand")
+		// console.log(Bobj)
+		Bobj.preventDefault();
+		var for_ans = $(this).attr("for")
+		// alert(for_ans)
+		$.post('/brand_filter',"id="+for_ans,function(res){
+			console.log(res)
+		})
+	})
+	$(".category").click(function(Bobj){
+		// alert("category");
+		Bobj.preventDefault();
+		var for_ans = $(this).attr("for")
+		// alert(for_ans);
+		$.post('/category_filter',"id="+for_ans,function(res){
+			// console.log(res);
+			if(res.length > 0){
+				htr = "";
+				for(i in res){
+					console.log(res[i])
+					htr+="<div class='col-sm-4'><div class='product-image-wrapper'><div class='single-products'><div class='productinfo text-center'><img src='/public/uploads/"+res[i].path+"' alt='' /><h2>"+res[i].price+"</h2><p>"+res[i].description+"</p><a href='#' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Add to cart</a></div><div class='product-overlay'><div class='overlay-content'><h2>$"+res[i].price+"</h2><p>"+res[i].description+"</p><a href='#' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Add to cart</a></div></div></div><div class='choose'><ul class='nav nav-pills nav-justified'><li><a href='#'><i class='fa fa-plus-square'></i>Add to wishlist</a></li><li><a href='#'><i class='fa fa-plus-square'></i>Add to compare</a></li></ul></div></div></div>"
+				}
+				$(".padding-right").html(htr)
+
+			}
+			else{
+				$(".padding-right").html("")
+			}
+		})
+	})
 	$(".btn_category").click(function(){
 		ans = $("#category_action").serialize();
 		// alert(ans)
